d7c8a344938b5e28284b0c1fd3b1c909f14ed9d3
semimono <culleneason@gmail.com>
2016-02-05 12:59:55 -0700

    Added awesome whamy-kablam.

---
+++ Assets/Scripts/Items/AssaultRifle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AssaultRifle : Item {
+
+	public float fireDelay = 0.1f;
+	public float inaccuracy = 0.1f;
+	public float range = 30;
+	public float damage = 10;
+	public float impulse = 1;
+
+	public override void invoke(Inventory invoker) {
+		Transform cam = invoker.getPlayer().cam.transform;
+
+		print("firing'!");
+		RaycastHit hitInfo;
+		bool hit = Physics.Raycast(cam.position, cam.forward, out hitInfo, range);
+		if (hit) {
+			Rigidbody rb = hitInfo.collider.GetComponent<Rigidbody>();
+            if (rb != null) {
+				print("huh?");
+				rb.AddForceAtPosition(cam.forward * impulse, hitInfo.point);
+			}
+		}
+	}
+}
