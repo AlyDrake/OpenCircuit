66c3b6ef556f0684bee0071df37f0ef2730ceb0d
michbr <bmichaud45@gmail.com>
2016-02-06 11:35:34 -0700

    Created the hunt action to support the new robot behaviour

---
+++ Assets/Scripts/Attributes/Endeavour/HuntAction.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HuntAction : Endeavour {
+
+	private Label target;
+
+	public HuntAction(RobotController controller, List<Goal> goals, Label target)
+		: base(controller, goals, target.labelHandle) {
+		this.target = target;
+		this.name = "hunt";
+		requiredComponents = new System.Type[] { typeof(HoverJet) };
+	}
+
+	public override bool canExecute() {
+		HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+		ZappyArms arms = controller.GetComponentInChildren<ZappyArms>();
+		return arms != null && !arms.hasTarget() && !target.hasTag(TagEnum.Grabbed) && jet != null && jet.canReach(target);
+	}
+
+	public override void execute() {
+		base.execute();
+		HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+		if(jet != null) {
+			jet.pursueTarget(target.labelHandle, false);
+			jet.setAvailability(false);
+		}
+	}
+
+	public override void stopExecution() {
+		base.stopExecution();
+		HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+		if(jet != null) {
+			jet.setTarget(null, false);
+			jet.setAvailability(true);
+		}
+	}
+
+	public override bool isStale() {
+		return !controller.knowsTarget(target.labelHandle);
+	}
+
+	public override void onMessage(RobotMessage message) {
+
+	}
+
+	protected override float getCost() {
+		HoverJet jet = controller.GetComponentInChildren<HoverJet>();
+		if(jet != null) {
+			return jet.calculatePathCost(target);
+		}
+		return 0;
+	}
+}
